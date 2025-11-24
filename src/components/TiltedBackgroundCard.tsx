0643ff7712f4de5ad7fcc73e7ce5a78f00ef39dd
Edward Tanguay <edward.tanguay@e-pixler.com>
2025-11-24 16:20:11 +0100

    expand finished

--- src/components/TiltedBackgroundCard.tsx
+++ src/components/TiltedBackgroundCard.tsx
@@ -11,10 +11,10 @@ export const TiltedBackgroundCard = ({
 		<div className="flex justify-center p-6">
 			<div className="relative">
 				{/* tilted background card */}
-				<div className="max-w-[16rem] min-h-[10rem] top-[.5rem] left-[.2rem] absolute inset-0 rounded-3xl bg-blue-800 opacity-20 -rotate-[4deg] scale-105 z-0"></div>
+				<div className="max-w-[16rem] top-[.5rem] left-[.2rem] absolute inset-0 rounded-xl bg-yellow-600 -rotate-[4deg] scale-105 z-0"></div>
 
 				{/* main card: blurs see-through opacity */}
-				<div className="relative max-w-[16.5rem] min-h-[10.5rem] rounded-[1.5rem] border border-white backdrop-blur-sm p-6 z-10">
+				<div className="relative max-w-[16.5rem] rounded-xl border border-white backdrop-blur-sm p-6 z-10">
 					{children}
 				</div>
 			</div>
