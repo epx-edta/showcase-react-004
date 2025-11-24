d3ce46f19a3faec1a92f7e63f28c0e0716db3592
Edward Tanguay <edward.tanguay@e-pixler.com>
2025-11-24 16:12:09 +0100

    three diferent heights

--- src/components/TiltedBackgroundCard.tsx
+++ src/components/TiltedBackgroundCard.tsx
@@ -11,10 +11,10 @@ export const TiltedBackgroundCard = ({
 		<div className="flex justify-center p-6">
 			<div className="relative">
 				{/* tilted background card */}
-				<div className="w-[16rem] h-[22.5rem] top-[.5rem] left-[.2rem] absolute inset-0 rounded-3xl bg-emerald-900 opacity-50 -rotate-[5deg] scale-105"></div>
+				<div className="max-w-[16rem] min-h-[10rem] top-[.5rem] left-[.2rem] absolute inset-0 rounded-3xl bg-blue-800 opacity-20 -rotate-[4deg] scale-105 z-0"></div>
 
 				{/* main card: blurs see-through opacity */}
-				<div className="relative w-[16.5rem] h-[23.5rem] rounded-[1.5rem] border border-white backdrop-blur-sm">
+				<div className="relative max-w-[16.5rem] min-h-[10.5rem] rounded-[1.5rem] border border-white backdrop-blur-sm p-6 z-10">
 					{children}
 				</div>
 			</div>
