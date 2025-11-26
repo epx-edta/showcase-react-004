ee009f80256b4237fc07e9e25464dbdcb35de012
Edward Tanguay <edward.tanguay@e-pixler.com>
2025-11-26 10:59:03 +0100

    fix background info

--- src/components/TiltedBackgroundCardSimple.tsx
+++ src/components/TiltedBackgroundCardSimple.tsx
@@ -19,7 +19,7 @@ export const TiltedBackgroundCardSimple = ({
 					{children}
 
 					{/* info icon */}
-					<div className="absolute top-[-18px] left-[40%] w-[30px] h-[40px] bg-green-900 rounded text-xs text-gray-300">
+					<div className="absolute bottom-[-18px] left-[140%] w-[30px] h-[30px] bg-green-900 rounded text-xs text-gray-300">
 						<BsInfoSquareFill className="text-[2rem]" />
 					</div>
 				</div>
