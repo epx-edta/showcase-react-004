97b6f8d7ff8fe042c82e5121eb0ccb7e5f53c1c3
Edward Tanguay <edward.tanguay@e-pixler.com>
2025-11-26 10:48:53 +0100

    reduced to 1

--- src/pages/PageAnimatedCard.tsx
+++ src/pages/PageAnimatedCard.tsx
@@ -5,17 +5,6 @@ const PageAnimatedCard: React.FC = () => {
   return (
 		<div className="flex gap-2 justify-center flex-wrap items-start">
 			<TiltedBackgroundCard>
-				<p>This is a test of this card.</p>
-			</TiltedBackgroundCard>
-			<TiltedBackgroundCard>
-				<p>
-					Lorem ipsum dolor sit amet consectetur adipisicing elit.
-					Ipsum sed omnis nulla facilis tempore aperiam perferendis
-					iusto, porro molestias autem ratione aliquam ducimus
-					distinctio rem cumque minus sint, maxime ut! is a test
-				</p>
-			</TiltedBackgroundCard>
-			<TiltedBackgroundCard>
 				<p>
 					Lorem ipsum dolor sit amet consectetur adipisicing elit.
 					Ipsum sed omnis nulla facilis tempore aperiam perferendis
