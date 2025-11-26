059213586ece235e572c4154f2934a510632e30f
Edward Tanguay <edward.tanguay@e-pixler.com>
2025-11-26 10:47:40 +0100

    moved from expand

--- src/pages/PageAnimatedCard.tsx
+++ src/pages/PageAnimatedCard.tsx
@@ -1,11 +1,33 @@
 import React from 'react';
+import { TiltedBackgroundCard } from '../components/TiltedBackgroundCard';
 
 const PageAnimatedCard: React.FC = () => {
   return (
-    <div className="animated-card">
-      <h1>Animated Card</h1>
-      <p>This is the AnimatedCard page.</p>
-    </div>
+		<div className="flex gap-2 justify-center flex-wrap items-start">
+			<TiltedBackgroundCard>
+				<p>This is a test of this card.</p>
+			</TiltedBackgroundCard>
+			<TiltedBackgroundCard>
+				<p>
+					Lorem ipsum dolor sit amet consectetur adipisicing elit.
+					Ipsum sed omnis nulla facilis tempore aperiam perferendis
+					iusto, porro molestias autem ratione aliquam ducimus
+					distinctio rem cumque minus sint, maxime ut! is a test
+				</p>
+			</TiltedBackgroundCard>
+			<TiltedBackgroundCard>
+				<p>
+					Lorem ipsum dolor sit amet consectetur adipisicing elit.
+					Ipsum sed omnis nulla facilis tempore aperiam perferendis
+					iusto, porro molestias autem ratione aliquam ducimus
+					distinctio rem cumque minus sint, maxime ut! is a test.
+					Lorem ipsum dolor sit amet consectetur adipisicing elit.
+					Odio nostrum praesentium doloremque commodi, numquam
+					incidunt porro debitis quam. Officiis nihil reiciendis
+					necessitatibus aut, illo delectus harum itaque iure quo eos.
+				</p>
+			</TiltedBackgroundCard>
+		</div>
   );
 };
 
