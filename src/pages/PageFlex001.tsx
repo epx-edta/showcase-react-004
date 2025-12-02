01ce878d9aae135212843ce22ba5a37bd47bc6f5
epx-edta <edward.tanguay@e-pixler.com>
2025-12-02 10:55:50 +0100

    FIX: Reduced flex to two sizes

--- src/pages/PageFlex001.tsx
+++ src/pages/PageFlex001.tsx
@@ -4,8 +4,8 @@ const PageFlex001: React.FC = () => {
   return (
     <div className="p-8">
       <h1 className="text-2xl font-bold mb-4">PageFlex001</h1>
-      <p>mobile=column, tablet=column, desktop=row</p>
-      <div className="flex gap-4 mt-6 flex-col md:flex-col 2xl:flex-row">
+      <p>mobile=column, tablet/desktop=row</p>
+      <div className="flex gap-4 mt-6 flex-col md:flex-row">
         <div className="bg-blue-300 p-4 rounded flex-1 text-center">Flex Item 1</div>
         <div className="bg-green-300 p-4 rounded flex-1 text-center">Flex Item 2</div>
       </div>
