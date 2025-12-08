02dfebb78ea0f92dfc73aead64693043055d6fe5
Edward Tanguay <edward.tanguay@e-pixler.com>
2025-12-08 11:55:16 +0100

    add auto flip feature

--- src/pages/PageImageViewer.tsx
+++ src/pages/PageImageViewer.tsx
@@ -1,4 +1,4 @@
-import { ImageStack } from '../components/ImageStack';
+import { ImageViewer } from '../components/ImageViewer';
 
 export const PageImageViewer = () => {
   const images = [
@@ -8,10 +8,10 @@ export const PageImageViewer = () => {
   ];
 
   return (
-      <div className="max-w-6xl mx-auto space-y-8">
-        <div className="bg-white/5 backdrop-blur-sm rounded-2xl p-8 shadow-2xl">
-          <ImageStack pathAndFileNames={images} />
-        </div>
+    <div className="max-w-6xl mx-auto space-y-8">
+      <div className="bg-white/5 backdrop-blur-sm rounded-2xl p-8 shadow-2xl">
+        <ImageViewer pathAndFileNames={images} />
+      </div>
     </div>
   );
 };
